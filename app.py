e9d72c3b12d6ca067df7c96500597e638cb46b28
valent4 <vg118664@gmail.com>
2025-04-24 09:47:49 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from streamlit_drawable canvas import st canvas
+
+st. title("Tablero para dibujo")
+
+with st. sidebar:
+  st. subheader ("Propiedades del Tablero")
+  drawing mode = st.sidebar.selectbox(
+    "Herramienta de Dibujo:",
+    ("freedraw","line", "rect", "circle", "transform", "polygon", "point"),
+)
+
+stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+stroke_color = st. color_picker("color de trazo", "#FFFFFF")
+og color = '#000000'
+
+# Create a canvas component
+canvas result = st_canvas(
+  fill_color="rgba(255, 165, 0, 0.3)", # Fixed fill color with some opacity
+  stroke_width=stroke_width,
+  stroke_color=stroke_color,
+  background_color=bg_color, 
+  height=300, width=500, 
+  drawing_mode=drawing mode,
+  key="canvas",
+)
